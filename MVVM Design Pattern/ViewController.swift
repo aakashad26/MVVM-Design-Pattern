6991cbefd42633887d21b5cc372556dca681900f
Aakash <aakash.ad26@gmail.com>
2020-06-18 10:55:48 -0700

    Initial Commit

---
+++ MVVM Design Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Design Pattern
+//
+//  Created by Aakash Adhikari on 6/18/20.
+//  Copyright © 2020 Aakash Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
